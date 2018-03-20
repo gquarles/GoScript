678c52e01d66ff225036d94bb82e708fb6c77fd6
Griffin <griffinq178@gmail.com>
2018-03-20 19:28:44 -0400

    Created an installer script which will clone the latest compile.lua from the repo

---
+++ installer.lua
@@ -0,0 +1,26 @@
+
+http.request("https://raw.githubusercontent.com/gquarles/goscript/master/compile.lua")
+
+local requesting = true
+
+while requesting do
+  local event, url, sourceText = os.pullEvent()
+  
+  if event == "http_success" then
+    respondedText = sourceText.readAll()
+    
+    sourceText.close()
+    
+    requesting = false
+    print("Cloned compiler from GitHub Repo at compiler.lua")
+
+    osfile = fs.open("compiler.lua", "w")
+    osfile.writeLine(respondedText)
+	osfile.close()
+
+  else
+    print("Error not connect to GitHub. Make sure your server allows HTTP requests.")
+    requesting = false
+  end
+end
+
